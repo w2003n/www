6ff1b8215a1539e8155869d771718e6365626a52
w2003n <59113153+w2003n@users.noreply.github.com>
2019-12-24 14:30:53 +0800

    Update App.java

--- demo02/src/main/java/com/git/App.java
+++ demo02/src/main/java/com/git/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("1234567890");
+        System.out.println("1234567854rwedsfghytredcvghre");
         System.out.println( "Hello World!" );
     }
 }

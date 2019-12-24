a0023bdbb638e7d6d4190e100a99a82bc257224c
w2003n <w2737879820@163.com>
2019-12-24 14:19:50 +0800

    提交

---
+++ demo02/src/main/java/com/git/App.java
@@ -0,0 +1,14 @@
+package com.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("1234567890");
+        System.out.println( "Hello World!" );
+    }
+}

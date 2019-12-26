75c1751a641b9e56c63b4d338b4c300e8806d277
w2003n <w2737879820@163.com>
2019-12-26 12:07:46 +0800

    dev

--- demo02/src/main/java/com/git/dev.java
+++ demo02/src/main/java/com/git/dev.java
@@ -2,7 +2,7 @@ package com.git;
 
 public class dev {
     public String getdev(){
-        System.out.println("dev_123");
+        System.out.println("master_123");
         System.out.println("dfdfdfd");
         return "456";
     }

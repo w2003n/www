41c9de0e4bd08c07a5c7bf8d0f89445e49b998d9
w2003n <w2737879820@163.com>
2019-12-26 10:02:42 +0800

    第三次提交文件

--- demo02/src/main/java/com/git/GetOne.java
+++ demo02/src/main/java/com/git/GetOne.java
@@ -3,6 +3,7 @@ package com.git;
 public class GetOne {
     public String getOne(){
         System.out.println("dfdfdfdfdfdfdf");
+        System.out.println("呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵呵");
         return "dfdf";
     }
 }

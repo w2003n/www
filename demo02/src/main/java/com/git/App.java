922ce1e52059ceaa9be49db00c4672946c4a16a0
w2003n <w2737879820@163.com>
2019-12-26 11:23:42 +0800

    dev提交

--- demo02/src/main/java/com/git/App.java
+++ demo02/src/main/java/com/git/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("1234567890");
-        System.out.println("1234567854rwedsfghytredcvghre");
-        System.out.println( "Hello World!" );
+        System.out.println("dev01");
     }
 }

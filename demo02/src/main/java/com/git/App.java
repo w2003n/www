c99f518ab8a83bb0e1abe7f5150efe3a61239a83
w2003n <w2737879820@163.com>
2019-12-26 11:32:28 +0800

    dev提交

--- demo02/src/main/java/com/git/App.java
+++ demo02/src/main/java/com/git/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("dfdfdfdfdfdfddfdf");
+        System.out.println("master01");
+        System.out.println("dev01");
     }
 }
